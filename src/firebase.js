43e4d62e3f855d11fdd5b3cb9f75ce10ddb6ebad
thdco <sarahsong10@naver.com>
2025-05-10 03:14:32 +0900

    로컬 작업 저장

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCoTpIf-FEXtZI2vqoUuxZSj7shRoHfhuo",
+    authDomain: "doa-friend-app-web.firebaseapp.com",
+    projectId: "doa-friend-app-web",
+    storageBucket: "doa-friend-app-web.firebasestorage.app",
+    messagingSenderId: "106268715001",
+    appId: "1:106268715001:web:4e705692f12c9501643f28",
+    measurementId: "G-E08KH5WLN7",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { analytics, db };
